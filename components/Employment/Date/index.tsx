e4a19678c5995eeade19d29bd07b60aecfcdabfd
Jun Joy <chojuneseo1@gmail.com>
2021-12-25 17:07:48 +0900

    UPDATE: change date list as the mode changes

--- components/Employment/Date/index.tsx
+++ components/Employment/Date/index.tsx
@@ -1,18 +1,31 @@
 import { FC } from "react";
 import * as S from "./styles";
 import * as M from "../styles";
+import { Mode } from "../../../states/atoms/Employment";
+import { useRecoilValue } from "recoil";
 
 const Date: FC = (): JSX.Element => {
+  const mode = useRecoilValue(Mode);
+
   return (
     <S.Container>
-      <M.Box>
-        <S.FilterName>파견일자</S.FilterName>
-        <S.Selection />
-      </M.Box>
-      <M.Box>
-        <S.FilterName>종료일자</S.FilterName>
-        <S.Selection />
-      </M.Box>
+      {mode ? (
+        <>
+          <M.Box>
+            <S.FilterName>파견일자</S.FilterName>
+            <S.Selection />
+          </M.Box>
+          <M.Box>
+            <S.FilterName>종료일자</S.FilterName>
+            <S.Selection />
+          </M.Box>
+        </>
+      ) : (
+        <M.Box>
+          <S.FilterName>근로 계약 일자</S.FilterName>
+          <S.Selection />
+        </M.Box>
+      )}
       <S.ChangeDate type="button" value="일자변경" />
     </S.Container>
   );
